6a6de3e8f881a3a060f9bb0b50c567eb67f7df2c
radixenggar <radixe@yahoo.com>
2015-02-03 01:25:48 +0800

    Android sync update

---
+++ Pos/src/com/android/pos/model/RequestBean.java
@@ -0,0 +1,17 @@
+package com.android.pos.model;
+
+import java.util.Date;
+
+public class RequestBean {
+
+	protected Date lastSyncDate;
+
+	public Date getLastSyncDate() {
+		return lastSyncDate;
+	}
+
+	public void setLastSyncDate(Date lastSyncDate) {
+		this.lastSyncDate = lastSyncDate;
+	}
+
+}
