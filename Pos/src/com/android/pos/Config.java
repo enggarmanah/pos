8d839aa03049ca2eca58c88cad79bb13a4230806
enggarmanah <radixe@yahoo.com>
2015-02-12 16:20:59 +0800

    Sync module update

--- Pos/src/com/android/pos/Config.java
+++ Pos/src/com/android/pos/Config.java
@@ -2,7 +2,7 @@ package com.android.pos;
 
 public class Config {
 	
-	public static String SERVER_URL = "http://192.168.0.103:8888";
-	//public static String SERVER_URL = "http://20.194.21.242:8888";
+	//public static String SERVER_URL = "http://192.168.0.103:8888";
+	public static String SERVER_URL = "http://20.194.21.242:8888";
 	//public static String SERVER_URL = "http://192.168.1.192:8888";
 }
